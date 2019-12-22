6a8f2645cf561b5171ca549cf879b28b1904803b
AlexS1995 <sarpe.1692247@studenti.uniroma1.it>
2019-12-22 19:37:34 +0100

    Fix

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 //Database setup
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => console.log("Connected to database"))
-.catch( err => console.error("Error"));
+.catch( err => console.error(process.env.MONGODB_URI));
 
 //Middleware setup
 app.use(express.urlencoded({extended: true}));
