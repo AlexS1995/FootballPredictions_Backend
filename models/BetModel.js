0c24a86975a1fc81692e3fdef5fb2f3451f776be
AlexS1995 <sarpe.1692247@studenti.uniroma1.it>
2020-02-16 14:53:51 +0100

    Debug bet model

--- models/BetModel.js
+++ models/BetModel.js
@@ -5,12 +5,12 @@ const Schema = mongoose.Schema;
 
 mongoose.set('useCreateIndex', true);
 
-const betSchema = new mongoose.Schema({
+const betSchema = new Schema({
     uid: {type: String, required: true},
     date: {type: String, required: true},
     matches: [{home: {type: String}, away: {type: String}, value: {type: String}}]
 });
 
-betSchema.plugin(uniqueValidator);
+//betSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('Bet', betSchema);
